1ca390b38a348ca6634391a1154069dc4ef22174
KangJingbo <hello.jingbo@gmail.com>
2022-06-23 10:36:22 +0800

    增加查询文件接口

--- src/main/java/com/bornya/pim/controller/SystemController.java
+++ src/main/java/com/bornya/pim/controller/SystemController.java
@@ -2,6 +2,7 @@ package com.bornya.pim.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
@@ -10,8 +11,8 @@ import java.io.*;
 @RequestMapping("/systemi")
 public class SystemController {
     @GetMapping("/runtime")
-    public String getSystemLog() throws IOException {
-        String logPath = "./nohup.out";
+    public String getSystemFile(@RequestParam(required = true) String f) throws IOException {
+        String logPath = "./" + f;
         FileReader reader = new FileReader(logPath);
         BufferedReader bufferedReader = new BufferedReader(reader);
 
